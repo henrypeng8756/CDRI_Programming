74c7596325713e9df3eada3ddf91f410637fe044
Henry Peng <henry.peng@dynasafe.com.tw>
2022-09-15 23:26:42 +0800

    Finish Python Exercise 6

---
+++ Python/Py_Projects/Exercise/Python_Exercise_6_12_彭文宏.py
@@ -0,0 +1,19 @@
+# %%
+def cleanstrList(dirtyList):
+    dirtyList = str(dirtyList)
+    dirtyList = dirtyList.strip('[]')
+    dirtyList = dirtyList.replace(',','')
+    print(dirtyList)
+
+numList=[]
+valueNum = eval(input('Enter a number: '))
+for valueNum in range(1,valueNum+1):
+    numList.append(valueNum)
+    cleanstrList(numList)
+fixList = [*range(1,valueNum+1)]
+cleanstrList(fixList)
+for valueNum in range(1,valueNum+1):
+    numList.pop()
+    cleanstrList(numList)
+
+# %%
