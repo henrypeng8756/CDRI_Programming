c39885f8e579f191d734974722d654191fded35b
Henry.Peng <henrypeng8756@outlook.com>
2022-09-16 14:35:46 +0800

    new way to run exe6

--- Python/Py_Projects/Exercise/Python_Exercise_6_12_彭文宏.py
+++ Python/Py_Projects/Exercise/Python_Exercise_6_12_彭文宏.py
@@ -17,3 +17,15 @@ for valueNum in range(1,valueNum+1):
     cleanstrList(numList)
 
 # %%
+itemNumber = eval(input('Enter a number: '))
+for i in range(1, itemNumber+1):
+    for j in range(1,i+1):
+        print(j, end=' ')
+    print('\n')
+
+for i in range(1, itemNumber+1):
+    for j in range(1,itemNumber+2-i):
+        print(j, end=' ')
+    print('\n')
+        
+# %%
