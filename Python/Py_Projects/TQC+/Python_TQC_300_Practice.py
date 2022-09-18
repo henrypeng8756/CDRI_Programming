af4e646b1a208c7fcaa9535a1ac1bf8c882c32fa
Henry Peng <henry.peng@dynasafe.com.tw>
2022-09-18 22:19:49 +0800

    finish 303 not performing well

--- Python/Py_Projects/TQC+/Python_TQC_300_Practice.py
+++ Python/Py_Projects/TQC+/Python_TQC_300_Practice.py
@@ -20,3 +20,16 @@ for i in range(sortList[0],sortList[1]+1):
 print(sum(evenList))
     
 # %%
+i = eval(input())  # i =2
+for line in range(1,i+1): # (1,3)
+    for numb in range(1,line+1): # (1,3)
+        print('%4d' %(line*numb), end ='')
+    print('\n')
+
+# %%
+
+# %%
+959/2
+
+
+# %%
