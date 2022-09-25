44ab59452c651eeb627eeb879d1ad531ae21ed87
Henry Peng <henry.peng@dynasafe.com.tw>
2022-09-25 21:47:21 +0800

    finish tqc 300 and start 400

---
+++ Python/Py_Projects/TQC+/Python_400_Practive.py
@@ -0,0 +1,19 @@
+# %% 
+def compute():
+    department = input()
+    studentID = input()
+    sName = input()
+    print('Department: %s'%(department))
+    print('Student ID: %s'%(studentID))
+    print('Name: %s'%(sName))
+compute()
+
+# %%
+x = eval(input())
+y = eval(input())
+def compute(a,b):
+    print('%d'%(a*b))
+compute(x,y)
+
+
+# %%
--- Python/Py_Projects/TQC+/Python_TQC_300_Practice.py
+++ Python/Py_Projects/TQC+/Python_TQC_300_Practice.py
@@ -1,4 +1,7 @@
 # %%
+import re
+
+
 a = eval(input())
 b = eval(input())
 sumList = []
@@ -56,9 +59,47 @@ print(numbSum)
 numb = input()
 print(numb[::-1])
 # %%
+numMulti = 1
+n = eval(input())
+for times in range(1,n+1):
+    numMulti *= times
+print(numMulti)
 
 # %%
+n = eval(input())
+for rows in range(1,n+1):
+    for columns in range(1,n+1):
+        column_str = '%d * %d = %-4d' %(columns,rows,rows*columns)
+        print('%s'%(column_str),end='')
+    print('')
 
 # %%
+num = []
+n = eval(input())
+for times in range(n):
+    num.clear()
+    num_str = input()
+    for str_split in num_str:
+        num.append(str_split)
+        int_num = list(map(int,num))
+    print(f'Sum of all digits of {num_str} is {sum(int_num)}')
+
+# %%
+amount = eval(input())
+returnRate = eval(input())
+months = eval(input())
+returnNum = 0
+print('%s \t  %s' % ('Month', 'Amount'))
+for annaulP in range(1,months+1):
+    amount += amount*returnRate/1200
+    print('%3d \t %.2f' % (annaulP, amount))
+
+# %%
+numSum = 0
+n = eval(input())
+for times in range(2,n+1):
+    numSum += 1/((times-1)**0.5+(times)**0.5)
+print('%.4f'%(numSum))
+
 
 # %%
