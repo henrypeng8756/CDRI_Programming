15af0f925731e2b9e606d526e44d40a2f0a577e2
Henry.Peng <henrypeng8756@outlook.com>
2022-09-26 19:19:16 +0800

    course 0926 finished

--- Python/Py_Projects/Exercise/Python_Exercise_10_12_彭文宏.py
+++ Python/Py_Projects/Exercise/Python_Exercise_10_12_彭文宏.py
@@ -5,8 +5,9 @@ fltred_list = []
 with open('Questions/Exercise_10/drugstore.json','r',encoding='utf_8') as jsonFile:
     content = json.load(jsonFile)
     for row in content:
-        if row[1] == '新北市': #and row['機構狀態'] == '開業':
-            json.dump(fltred_list,row,ensure_ascii=False)
-            print(fltred_list)
+        if row[0]['機構狀態'] == '開業' and row[2]['地址縣市別'] == '新北市':
+            fltred_list.append('%s\t%s%s%s'%(row[1]['機構名稱'],row[2]['地址縣市別'],row[3]['地址鄉鎮市區'],row[4]['地址街道巷弄號']))
+for show_row in fltred_list[10:20]:
+    print(show_row)
 
 # %%
--- Python/Py_Projects/Python_Examples.py
+++ Python/Py_Projects/Python_Examples.py
@@ -1398,12 +1398,65 @@ data = '''<html><head><title>The Dormouse's story</title></head> <body> <p class
 '''
 from bs4 import BeautifulSoup as bs
 soup = bs(data,'lxml')
-print(soup)
+#print(soup)
 print(type(soup))
 # print(data.prettify()) # wrong 
-print(soup.prettify())
+#print(soup.prettify())
 print(soup.b.prettify())
+print(soup.p.prettify())
+print(soup.a)
+print(soup.find('a'))
+print(soup.find('a').text)
+print(soup.find(id='link1').text)
+#print(soup.find(class='link1').text) #wrong
+print(soup.find(class_='title').text)
+print(soup.find(class_='sister').text)
+print(soup.find(href='http://example.com/tillie').text)
 print(soup.p.text)
 print(soup.title.string)
+
+# %%
+data = '''<html><head><title>The Dormouse's story</title></head> <body> <p class="title"><b>The Dormouse's story</b></p> <p class="story">Once upon a time there were three little sisters; and their names were <a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>, <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>; and they lived at the bottom of a well.</p> <p class="story">...</p>
+
+'''
+soup = bs(data, 'lxml')
+print(soup.title)
+print(soup.head)
+print(soup.head.title)
+print(soup.title.string)
+print(soup.find_all('p'))
+for link in soup.find_all('a'):
+      print(link.get('href'))
+for className in soup.find_all('p'):
+      print(className.get('class'))
+print(soup.prettify)
+
+# %%
+from bs4 import BeautifulSoup
+html_text = '''
+<html><head><title>國立臺灣大學系統</title></head>
+<body>
+<p class="title"><b>三校聯盟 NTU SYSTEM</b></p>
+<p class="ntu_system">
+<a href="http://www.ntu.edu.tw" class="union" id="link1">臺灣大學</a>
+<a href="http://www.ntnu.edu.tw" class="union" id="link2">臺灣師範大學</a>
+<a href="http://www.ntust.edu.tw" class="union" id="link3">臺灣科技大學</a>
+</p></body></html>
+'''
+bs = BeautifulSoup(html_text,'html.parser')
+print('1:',bs.title)
+print('2:',bs.find('a'))
+print('3:',bs.find('b'))
+print('4:',bs.find_all('a',{'class':'union'}))
+print('5:',bs.find('a',{'id':'link1'}))
+print('6:',bs.find('a',{'href':'http://www.ntust.edu.tw'}))
+web = bs.find('a',{'id':'link1'})
+print('7_1:',web)
+print('7:',web.get('href'))
+data = bs.select('.union') # due to CSS | .union == class='union' | . == class= |
+print('8_1:', data)
+print('8:',data[0].text)
+print('9:',data[1].text)
+print('10:',bs.select("#link3")) # due to CSS | #link3 == id='link3' | # == id= |
 
 # %%
