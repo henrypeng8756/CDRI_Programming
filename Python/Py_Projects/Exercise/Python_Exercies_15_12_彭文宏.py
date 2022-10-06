5b15c4567a7a4206e1261b025b09d714a75649d7
Henry Peng <henry.peng@dynasafe.com.tw>
2022-10-06 21:21:18 +0800

    commit from mac pro

---
+++ Python/Py_Projects/Exercise/Python_Exercies_15_12_彭文宏.py
@@ -0,0 +1,24 @@
+# %%
+import numpy as np
+ans = np.random.randint(1,13) #5
+K = 13
+Q = 12
+J = 11
+A = 1
+cardList = 2
+guess = input(print('猜猜看撲克牌數字是多少?')) #6
+while True:
+    if guess not in cardList:
+        print('撲克牌只有A-10,J,Q,K 請重新輸入')
+    elif guess == ans:
+        print('恭喜你！猜對了～')
+    elif 
+    
+# %%
+a = [1,2,3]
+g = input()
+if g != a:
+    print('not in')
+else:
+    print('wrong')
+# %%
